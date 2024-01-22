2c893fa9f2977c47a2976bebe4a72d532e2e4cbe
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2024-01-22 14:30:00 +0300

    remove redundant code

--- scripts/page_available_since.py
+++ scripts/page_available_since.py
@@ -1,5 +1,4 @@
 import requests
-import re
 import json
 
 versions = [
@@ -73,13 +72,6 @@ def process_and_write_to_file() -> None:
         json.dump(data_file, file, indent=4)
 
 
-def write_aliases():
-    with open("aliases.txt", 'w') as file:
-        ns = sorted(aliases)
-        for item in ns:
-            file.write(str(item) + '\n')
-
-
 def get_and_process_urls():
     available_since = {}
     for version in versions:
@@ -118,45 +110,14 @@ def get_and_process_urls():
     return dict(sorted(available_since.items()))
 
 
-def replace_base_url(url: str) -> str:
-    version_pattern = r'https://tyk\.io/docs/[0-9]+(\.[0-9]+)?'
-    replace_nightly = 'https://tyk.io/docs/nightly'
-    replace_3lts = 'https://tyk.io/docs/3-lts'
-    replace_latest = 'https://tyk.io/docs'
-    modified_url = re.sub(version_pattern, '', url)
-    modified_url = modified_url.replace(replace_nightly, '')
-    modified_url = modified_url.replace(replace_3lts, '')
-    modified_url = modified_url.replace(replace_latest, '')
-    return modified_url
-
-
-def read_file(file_name: str):
-    with open(file_name, 'r') as json_file:
-        data = json.load(json_file)
-    return data
-
-
 def fetch_file(url: str):
-    print("Getting sitemap for {url}".format(url=url))
+    print("Getting pagesurl.json for {url}".format(url=url))
     response = requests.get(url, headers={'user-agent': 'insomnia/2023.4.0'})
     if response.status_code != 200:
         raise Exception("unable to fetch the pagesurl.json")
     response.raise_for_status()
     print("finished fetching for {url}".format(url=url))
     return response.json()
-
-
-def test_modified():
-    urls = [
-        'https://tyk.io/docs/nightly/universal-data-graph/udg-getting-started/security/',
-        'https://tyk.io/docs/5.2/universal-data-graph/udg-getting-started/security/',
-        'https://tyk.io/docs/5.1/apim/',
-        'https://tyk.io/docs/3.0/apim/test/is/it/docs/nightly',
-        'https://tyk.io/docs/4.2/apim/itachi',
-        'https://tyk.io/docs/getting-started/'
-    ]
-    for url in urls:
-        print(replace_base_url(url))
 
 
 process_and_write_to_file()
